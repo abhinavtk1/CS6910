1a78e1cdddb18cd8bd4b9b7c9446e1a1d672ee53
abhinavtk1 <ma23m002@smail.iitm.ac.in>
2024-03-02 14:47:36 +0530

    added class_plot.py

---
+++ class_plot.py
@@ -0,0 +1,26 @@
+# import fashion-mnist dataset using keras and plot on image for each class
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.datasets import fashion_mnist
+
+# Load Fashion-MNIST dataset
+(x_train, y_train), (_, _) = fashion_mnist.load_data()      # fashion_mnist documentation - https://keras.io/api/datasets/fashion_mnist/ 
+
+# Class names - the index of the class names corresponds to the class label
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# Function to plot one sample image for each class
+
+def plot_samples(x, y, class_names):
+    num_classes = len(class_names)
+    fig, axes = plt.subplots(1, num_classes, figsize=(20, 5))
+    for i in range(num_classes):
+        idx = np.where(y == i)[0][0]
+        axes[i].imshow(x[idx], cmap='gray')
+        axes[i].set_title(class_names[i])
+        axes[i].axis('off')
+    plt.show()
+
+# Plot one sample image for each class
+plot_samples(x_train, y_train, class_names)
