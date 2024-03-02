3d7b083d4b82763d8b0abbf5d712a0c3903b95a6
abhinavtk1 <ma23m002@smail.iitm.ac.in>
2024-03-02 12:29:18 +0530

    Q1 done

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CS6910
 CS6910 - Assignment 1 
+
+Project update - 
+1. Downloaded Fashion MNIST dataset using keras and plotted one image of each class.
+2. Building feedforward architecture...
--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 from keras.datasets import fashion_mnist
 
 # Load Fashion-MNIST dataset
-(x_train, y_train), (_, _) = fashion_mnist.load_data()
+(x_train, y_train), (_, _) = fashion_mnist.load_data()      # https://keras.io/api/datasets/fashion_mnist/ fashion_mnist documentation
 
-# Define class names
+# Class names - the index of the class names corresponds to the class label
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 # Function to plot one sample image for each class
+'''
 def plot_samples(x, y, class_names):
     num_classes = len(class_names)
     fig, axes = plt.subplots(1, num_classes, figsize=(20, 5))
@@ -19,6 +20,8 @@ def plot_samples(x, y, class_names):
         axes[i].set_title(class_names[i])
         axes[i].axis('off')
     plt.show()
-
+'''
 # Plot one sample image for each class
-plot_samples(x_train, y_train, class_names)
+#plot_samples(x_train, y_train, class_names)
+
+print(x_train[0][15])
